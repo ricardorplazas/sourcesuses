547d9b87077f6b5404ccd9f0178de53bd340438c
Ricardo <ricardo.rplazas@gmail.com>
2025-04-25 11:52:06 +0200

    Quartz sync: Apr 25, 2025, 11:52 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -91,7 +91,7 @@ const config: QuartzConfig = {
       Plugin.Assets(),
       Plugin.Static(),
       Plugin.NotFoundPage(),
-      Plugin.CNAME(),
+      // Plugin.CNAME(),
       // Comment out CustomOgImages to speed up build time
       // Plugin.CustomOgImages(),
     ],
